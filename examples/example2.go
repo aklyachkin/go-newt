85ba63404f8743d5b7a9aaed1b4d0dc3c176635f
aklyachkin <aklyachkin@gmail.com>
2014-07-09 15:33:16 +0200

    example2 added

---
+++ examples/example2.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/aklyachkin/go-newt"
+
+func main() {
+    newt.Init()
+    newt.Cls()
+
+    newt.OpenWindow(10, 5, 40, 6, "Button sample")
+
+    b1 := newt.Button(10, 1, "Ok")
+    b2 := newt.CompactButton(22, 2, "Cancel")
+    form := newt.Form(nil, nil, 0)
+    newt.FormAddComponent(form, b1)
+    newt.FormAddComponent(form, b2)
+
+    newt.RunForm(form)
+
+    newt.FormDestroy(form)
+    newt.Finished()
+}
